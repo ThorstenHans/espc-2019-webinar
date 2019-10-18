11ea59e636865618462e9f6d2510d585656c9e52
Thorsten Hans <thorsten.hans@gmail.com>
2019-10-18 11:30:26 +0200

    feat: live coding at 11:30

--- main.tf
+++ main.tf
@@ -12,3 +12,18 @@ resource "azurerm_resource_group" "espcrg1" {
   location = "westus"
   tags     = "${local.all_tags}"
 }
+
+resource "azurerm_app_service_plan" "espcasp1" {
+  name                = "asp-espc-webinar-live"
+  resource_group_name = "${azurerm_resource_group.espcrg1.name}"
+  location            = "${azurerm_resource_group.espcrg1.location}"
+
+  kind     = "Linux"
+  reserved = true
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+  tags = "${local.all_tags}"
+}
